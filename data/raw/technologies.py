db608e6c87fe1d404ef15f05bc2866dd12d23c83
Muhammad-Jarrar-shaf <jarrarshaf74@gmail.com>
2026-06-09 12:41:14 +0500

    Meaningful message

---
+++ data/raw/technologies.py
@@ -0,0 +1,32 @@
+LANGUAGES = [
+    "Python",
+    "Java",
+    "JavaScript",
+    "Go",
+    "C++",
+]
+
+FRAMEWORKS = [
+    "FastAPI",
+    "Django",
+    "Flask",
+    "React",
+]
+
+DATABASES = [
+    "PostgreSQL",
+    "MongoDB",
+    "MySQL",
+]
+
+CLOUDS = [
+    "AWS",
+    "Azure",
+    "GCP",
+]
+
+DEVOPS = [
+    "Docker",
+    "Kubernetes",
+    "Jenkins",
+]
